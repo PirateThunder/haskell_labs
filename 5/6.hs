41dd8111237a050aaa7a4dac6f1499048b0103d3
ilyakhakimov03 <Khakimov.IA@servionica.ru>
2024-10-03 11:45:26 +0500

    fixed lab 5 task 6

--- 5/6.hs
+++ 5/6.hs
@@ -1,6 +1,6 @@
 data Vector2D x = Vector2D x x deriving (Show)
 
-scalar (Vector2D x1 y1) (Vector2D x2 y2) = Vector2D (x1*x2) (y1*y2)
+scalar (Vector2D x1 y1) (Vector2D x2 y2) = x1*x2 + y1*y2
 
 add (Vector2D x1 y1) (Vector2D x2 y2) = Vector2D (x1+x2) (y1+y2)
 
